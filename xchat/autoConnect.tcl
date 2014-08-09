2accba9f005a56568766c57a5537391b73da90bb
Perry Bhandal <perry.bhandal@gmail.com>
2014-08-08 20:28:17 -0400

    Added xchat dir

---
+++ xchat/autoConnect.tcl
@@ -0,0 +1,12 @@
+/timer 3 msg nickserv identify hockey
+/timer 3 set irc_conf_mode 1
+/timer 10 join #programming
+/timer 10 join #C++
+/timer 10 join #C
+/timer 10 join #java
+/timer 10 join #python
+/timer 10 join #ruby
+/timer 10 join #linux
+/timer 10 join #rubyonrails
+/timer 10 join #sql
+/timer 10 join #vim
