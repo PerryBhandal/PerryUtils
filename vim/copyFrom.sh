3be735684a591e75700c281588150c06ed9e7962
Perry Bhandal <perry.bhandal@gmail.com>
2014-08-16 20:25:07 -0400

    Added colors folder to copyFrom and copyTo, as well as the colors themselves.

--- vim/copyFrom.sh
+++ vim/copyFrom.sh
@@ -1,3 +1,4 @@
 cp -r ~/.vim/ftplugin .
+cp -r ~/.vim/colors colors
 cp ~/.vimrc REMOVE.vimrc
 cp ~/.ycm_extra_conf.py REMOVE.ycm_extra_conf.py
--- vim/copyTo.sh
+++ vim/copyTo.sh
@@ -1,3 +1,4 @@
 cp -r ftplugin ~/.vim/
+cp -r colors ~/.vim/
 cp REMOVE.vimrc ~/.vimrc
 cp REMOVE.ycm_extra_conf.py ~/.ycm_extra_conf.py
