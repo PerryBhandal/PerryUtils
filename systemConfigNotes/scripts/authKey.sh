09f5b39588f62db840f9eb8c30a3f221f1602ffc
Perry Bhandal <perry.bhandal@gmail.com>
2014-09-06 13:57:24 -0400

    Added ssh folder creation for all users.

--- systemConfigNotes/scripts/authKey.sh
+++ systemConfigNotes/scripts/authKey.sh
@@ -1,5 +1,1 @@
-if [ "$2" == "root" ]; then
-    cat ~/.ssh/id_rsa.pub | ssh $2@$1 'mkdir -p /root/.ssh; cat >> ~/.ssh/authorized_keys'
-else
-    cat ~/.ssh/id_rsa.pub | ssh $2@$1 'cat >> ~/.ssh/authorized_keys'
-fi
+cat ~/.ssh/id_rsa.pub | ssh $2@$1 'mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys'
