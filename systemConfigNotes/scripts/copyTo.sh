76f0edf64799e0e17e215bd78828ba99843f1ec7
Perry Bhandal <perry.bhandal@gmail.com>
2014-09-06 07:49:29 -0400

    Removed stiler, now using my own snapper.

--- systemConfigNotes/scripts/copyTo.sh
+++ systemConfigNotes/scripts/copyTo.sh
@@ -1,2 +1,9 @@
 mkdir -p ~/.scripts
-cp stiler.py ~/.scripts/
+
+pushd "~/.scripts" > /dev/null
+
+wget https://raw.githubusercontent.com/PerryBhandal/WindowSnapper/master/windowSnapper.rb
+
+chmod +x windowSnapper.rb
+
+popd > /dev/null
