06e9a805735560ee984d983ee34c97d0e5e6ec2b
Perry Bhandal <perry.bhandal@gmail.com>
2014-09-06 13:54:34 -0400

    Added ssh dir creation for root logins.

--- systemConfigNotes/scripts/authKey.sh
+++ systemConfigNotes/scripts/authKey.sh
@@ -1,1 +1,5 @@
-cat ~/.ssh/id_rsa.pub | ssh $2@$1 'cat >> ~/.ssh/authorized_keys'
+if [ "$2" == "root" ]; then
+    cat ~/.ssh/id_rsa.pub | ssh $2@$1 'mkdir -p /root/.ssh; cat >> ~/.ssh/authorized_keys'
+else
+    cat ~/.ssh/id_rsa.pub | ssh $2@$1 'cat >> ~/.ssh/authorized_keys'
+fi
