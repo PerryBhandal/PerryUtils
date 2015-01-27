e08e4a6647275390be314f23566c04cbfea7df7f
Perry Bhandal <perry.bhandal@gmail.com>
2015-01-27 10:26:49 -0500

    Added push option to gitnocomment.

--- systemConfigNotes/scripts/gitNoComment.sh
+++ systemConfigNotes/scripts/gitNoComment.sh
@@ -2,5 +2,9 @@ echo "Are you sure you want to do a no comment commit?"
 echo "Only do this in junk repos. Is this a junk repo?!"
 echo "Press ENTER to proceed with this commit." 
 read ENTERINPUT 
+
 git commit -a -m "No commit message."
+
+if [ -z $1 ]; then echo "Not pushing, no arg passed."; else git push;  fi
+
 
