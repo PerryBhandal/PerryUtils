1812c32522e14bb50f20c35027f6f19ff7a46684
Perry Bhandal <perry.bhandal@gmail.com>
2015-03-02 18:35:10 -0500

    Added multi resolution support.

--- systemConfigNotes/scripts/site_snap.rb
+++ systemConfigNotes/scripts/site_snap.rb
@@ -2,13 +2,29 @@ if ARGV[0] == nil
   puts "No domain supplied."
   exit
 end
+RESOLUTIONS = 
+[
+  [1920,1080],
+  [1366, 768],
+  [720,1280],
+  [320,480]
+]
+
 OUT_DIR = "/home/perry/work/git/attractive_sites"
 OUT_NAME = "#{Time.now.to_i}"
 
+def takeShot(folderName, xRes, yRes)
+  system("cutycapt --url=#{ARGV[0]} --out=#{folderName}/#{xRes}_#{yRes}.jpg --min-width=#{xRes} --min-height=#{yRes}")
+end
+
 Dir.chdir(OUT_DIR) do
-  system("cutycapt --url=#{ARGV[0]} --out=#{Time.now.to_i}.jpg --min-width=1920 --min-height=1080")
-  logFile = File.new("#{OUT_NAME}.txt", "w+")
-  logFile.puts("#{Time.now} - Took picture of #{ARGV[0]} and saved it to #{OUT_NAME}.jpg")
+  system("mkdir #{OUT_NAME}")
+  logFile = File.new("#{OUT_NAME}/log.txt", "w+")
+  logFile.puts("#{Time.now} - Took picture of #{ARGV[0]}")
+
+  RESOLUTIONS.each do |res|
+    takeShot(OUT_NAME, res[0], res[1])
+  end
 
   if ARGV[1] != nil
     logFile.puts("Reason for pic: #{ARGV[1..-1].join(" ")}")
@@ -17,5 +33,5 @@ Dir.chdir(OUT_DIR) do
   end
   logFile.close()
 
-  system("git pull; git add #{OUT_NAME}.*; git commit -a -m \"Image commit.\"; git push")
+  system("git pull; git add #{OUT_NAME}; git commit -a -m \"Image commit.\"; git push")
 end
