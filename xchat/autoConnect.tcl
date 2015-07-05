d6b2d6cbdb48abe0a38382019daa94e78cfd4006
Perry Bhandal <perry.bhandal@gmail.com>
2015-07-05 12:07:37 -0400

    Upped connect delay for IRC auto connect.

--- xchat/autoConnect.tcl
+++ xchat/autoConnect.tcl
@@ -1,14 +1,14 @@
-/timer 3 msg nickserv identify hockey
-/timer 3 set irc_conf_mode 1
-/timer 10 join #programming
-/timer 10 join #C++
-/timer 10 join #C
-/timer 10 join #java
-/timer 10 join #python
-/timer 10 join #ruby
-/timer 10 join #linux
-/timer 10 join #rubyonrails
-/timer 10 join #sql
-/timer 10 join #vim
-/timer 10 join ##c++-general
-/timer 10 join ##c++-basic
+/timer 10 msg nickserv identify hockey
+/timer 10 set irc_conf_mode 1
+/timer 20 join #programming
+/timer 20 join #C++
+/timer 20 join #C
+/timer 20 join #java
+/timer 20 join #python
+/timer 20 join #ruby
+/timer 20 join #linux
+/timer 20 join #rubyonrails
+/timer 20 join #sql
+/timer 20 join #vim
+/timer 20 join ##c++-general
+/timer 20 join ##c++-basic
---
+++ xchat/copyTo.sh
@@ -0,0 +1,1 @@
+cp autoConnect.tcl ~/.xchat2/
