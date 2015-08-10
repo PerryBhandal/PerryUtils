d2f5b5b8fd08aa3f5fd653513e7c0efb58e71c06
Perry Bhandal <perry.bhandal@gmail.com>
2015-08-10 18:25:32 -0400

    Upped start delay to resolve issue with scripts running before server connection completed.

--- xchat/autoConnect.tcl
+++ xchat/autoConnect.tcl
@@ -1,14 +1,14 @@
-/timer 10 msg nickserv identify hockey
-/timer 10 set irc_conf_mode 1
-/timer 20 join #programming
-/timer 20 join #C++
-/timer 20 join #C
-/timer 20 join #java
-/timer 20 join #python
-/timer 20 join #ruby
-/timer 20 join #linux
-/timer 20 join #rubyonrails
-/timer 20 join #sql
-/timer 20 join #vim
-/timer 20 join ##c++-general
-/timer 20 join ##c++-basic
+/timer 20 msg nickserv identify hockey
+/timer 20 set irc_conf_mode 1
+/timer 40 join #programming
+/timer 40 join #C++
+/timer 40 join #C
+/timer 40 join #java
+/timer 40 join #python
+/timer 40 join #ruby
+/timer 40 join #linux
+/timer 40 join #rubyonrails
+/timer 40 join #sql
+/timer 40 join #vim
+/timer 40 join ##c++-general
+/timer 40 join ##c++-basic
