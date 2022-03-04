77431d90757b53fcd016a133bfbc0f5ab37d0241
Perry Bhandal <perry.bhandal@gmail.com>
2022-03-04 12:09:37 -0800

    Removed unused vim files from copy.

--- vim/copyTo.sh
+++ vim/copyTo.sh
@@ -1,6 +1,6 @@
 cp -r ftplugin ~/.vim/
 cp -r colors ~/.vim/
 cp REMOVE.vimrc ~/.vimrc
-cp REMOVE.ideavimrc ~/.ideavimrc
+#cp REMOVE.ideavimrc ~/.ideavimrc
 cp REMOVE.ycm_extra_conf.py ~/.ycm_extra_conf.py
-cp REMOVE.vrapperrc ~/.vrapperrc
+#cp REMOVE.vrapperrc ~/.vrapperrc
