e5fa045c65d256df82f83ad0f2cb4c870aef59c2
Perry Bhandal <perry.bhandal@gmail.com>
2022-03-31 08:29:13 -0700

    Swapped to using gnome-screenshot instead of mate-screenshot.

--- arch/scripts/take_screenshot.sh
+++ arch/scripts/take_screenshot.sh
@@ -1,2 +1,2 @@
 sleep 0.1
-source /home/perry/.bashrc; rm -f /tmp/cur_shot.png; mate-screenshot -a -f /tmp/cur_shot.png; xclip -i -selection c -t image/png /tmp/cur_shot.png;
+source /home/perry/.bashrc; rm -f /tmp/cur_shot.png; gnome-screenshot -a -f /tmp/cur_shot.png; xclip -i -selection c -t image/png /tmp/cur_shot.png;
